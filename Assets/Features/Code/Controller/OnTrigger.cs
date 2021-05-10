25e2b087f77580b6ed1b9949b61f0956f316dd56
kfaoofyrbzcbfdadfj <kfaoofyrbzcbfdadfj@twzhhq.online>
2021-05-10 15:42:14 +0200

    #debut level design

--- Assets/Features/Code/Controller/OnTrigger.cs
+++ Assets/Features/Code/Controller/OnTrigger.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class OnTrigger : MonoBehaviour
 {
+    private Rigidbody _rb;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("uncube"))
+        if (other.gameObject.CompareTag("Parachute"))
         {
-            Debug.Log($"i hit that biatch ");
+            Debug.Log($"parachute");
+
+            _rb.velocity = new Vector3(0, 10, 0);
+        }
+
+        if (other.gameObject.CompareTag("Background"))
+        {
         }
     }
 }
---
+++ Assets/Features/Code/ScriptableObject/PlayerData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerData", menuName = "ScriptableObjects/Player")]
+public class PlayerData : ScriptableObject
+{
+    public string playerName;
+    public float score;
+}
